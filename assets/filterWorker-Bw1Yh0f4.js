071488138c00cfc36efa369008c6120daa21cafa
Yevhen Oliynyk <i.yevhen.oliynyk@gmail.com>
2025-03-04 12:40:09 +0100

    Updates

--- assets/filterWorker-Bw1Yh0f4.js
+++
@@ -1,1 +0,0 @@
-function r(e){return new Worker("/assets/filterWorker-CyJGooCJ.js",{name:e==null?void 0:e.name})}export{r as default};
---
+++ assets/filterWorker-DFIJUUnX.js
@@ -0,0 +1,1 @@
+function r(e){return new Worker("/ev-dashboard/assets/filterWorker-CyJGooCJ.js",{name:e==null?void 0:e.name})}export{r as default};
